bb20801b693a8854af33f25b37f7e96a07e0bea1
Venkata A Surapaneni <venkataasurapaneni@Venkatas-MacBook-Pro.local>
2024-10-24 16:48:52 +0530

    successful login update

--- cypress/e2e/tests/login.cy.js
+++ cypress/e2e/tests/login.cy.js
@@ -1,11 +1,22 @@
 import { loginPage } from "../../pages/loginPage"
 
 const loginObj = new loginPage()
+import logindata from "../../fixtures/logindata.json"
 
 describe('Logging in', () => {
 
-    it('No details entered', () => {
+    beforeEach('logging in', () => {
         cy.visit('login')
+    })
+
+    it('No details entered', () => {
         loginObj.clicksubmit()
+    })
+
+    it('Able to login successfully', () => {
+        loginObj.enterUsername(logindata.username)
+        loginObj.enterPassword(logindata.password)
+        loginObj.clicksubmit()
+        cy.get('#content').should('be.visible').and('contain', 'My Account')
     })
 })
